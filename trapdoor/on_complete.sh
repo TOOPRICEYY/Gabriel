667bc1bfea70ac42c7d7f804f18c584d0a9941e6
mittenmade <starfinderjetpacks@gmail.com>
2026-02-07 23:34:46 +0000

    Trapdoor opened

--- trapdoor/on_complete.sh
+++ trapdoor/on_complete.sh
@@ -3,13 +3,7 @@
 # This script is executed when the countdown reaches zero
 # Add your custom commands below
 
+git merge dev
 git commit -am "Trapdoor opened"
 git push origin main
 
-# Example: Send a notification (uncomment and modify as needed)
-# osascript -e 'display notification "Countdown finished!" with title "Timer"'
-
-# Example: Play a sound (uncomment and modify as needed)
-# afplay /System/Library/Sounds/Glass.aiff
-
-# Add your custom actions here
