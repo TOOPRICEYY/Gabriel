d5225d20f99b2264c92b55aa45997acfead8a3da
mittenmade <starfinderjetpacks@gmail.com>
2026-02-07 23:37:17 +0000

    Trapdoor opened

--- trapdoor/on_complete.sh
+++ trapdoor/on_complete.sh
@@ -3,7 +3,7 @@
 # This script is executed when the countdown reaches zero
 # Add your custom commands below
 
-git pull
+git pull --no-edit
 git merge dev
 git commit -am "Trapdoor opened"
 git push origin main
